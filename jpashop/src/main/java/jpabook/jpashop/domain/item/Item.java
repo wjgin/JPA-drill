9438e95d0eccc3f30853017816114ce877670a86
wjgin <woojin0913@naver.com>
2022-04-20 11:42:50 +0900

    add core business logic in Item Class this logic can change stockQuantity field it's better than using Setter method, more TDD

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,8 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id @GeneratedValue
@@ -25,4 +25,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //==비지니스 로직==//
+    // 외부에서 setter를 이용한 값 변경이 아닌 객체 안에서 핵심 비지니르 로직을 만들어 변경하는 것이 객체지향스러운 설계
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int result = this.stockQuantity - quantity;
+        if (result < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = result;
+    }
+
 }
